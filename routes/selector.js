0c4258ae6f01ce1b6177ed6e391d2a98497659fa
ChandrikaAnnadanam <chandrikaannadanam@gmail.com>
2021-11-03 19:55:38 -0500

    added images

--- routes/selector.js
+++ routes/selector.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Eagle', { title: 'Search Results by Eagle' });
+  res.render('selector', { title: 'Selector' });
 });
 
 module.exports = router;
